608626a2b41ddd0908e6a5019e45a36b740cad77
TontByte <tonycen957@gmail.com>
2026-05-02 17:58:18 -0600

    escritura de atributos basicos (y comentarios) para la clase area

--- Area.h
+++ Area.h
@@ -1,5 +1,22 @@
 #pragma once
-class Area
-{
+#include <iostream>
+#include <stdexcept>
+#include <string>
+#include "Ventanilla.h"
+
+using std::cout;
+using std::runtime_error;
+using std::endl;
+using std::string;
+
+class Area{
+private:
+	string descripcion;
+	string codigo;
+	//agregar eds para poner atributo ventanillas
+	//agregar minheap para cola de prioridad
+
+
+public:
 };
 
