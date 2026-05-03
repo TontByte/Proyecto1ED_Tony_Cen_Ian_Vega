5968706f39d517c4f3cd47629e27e8c434a7b918
TontByte <tonycen957@gmail.com>
2026-05-02 18:00:18 -0600

    escritura de los atributos de la clase ventanilla

--- Ventanilla.h
+++ Ventanilla.h
@@ -1,5 +1,19 @@
 #pragma once
-class Ventanilla
-{
+#include <iostream>
+#include <stdexcept>
+#include <string>
+#include "Ventanilla.h"
+
+using std::cout;
+using std::runtime_error;
+using std::endl;
+using std::string;
+
+class Ventanilla{
+private:
+	string nombre;
+	//creo que solo es esto?
+
+public:
 };
 
