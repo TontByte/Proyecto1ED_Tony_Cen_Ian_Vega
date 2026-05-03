569f55f1c824f9ef16d19a279f2e0cd96fd70ab1
TontByte <tonycen957@gmail.com>
2026-05-03 15:34:51 -0600

    Se agregan las clases PriorityQueue, HeapPriorityQueue y Pair

---
+++ HeapPriorityQueue.h
@@ -0,0 +1,58 @@
+#pragma once
+#include <iostream>
+#include <stdexcept>
+#include "PriorityQueue.h"
+#include "Pair.h"
+#include "MinHeap.h"
+#include "Util.h"
+
+using std::cout;
+using std::endl;
+using std::runtime_error;
+
+template <typename E>
+class HeapPriorityQueue : public PriorityQueue<E> { //agregar que crezca x2 cuando llega al limite
+private:
+	MinHeap<Pair<int, E>>* pairs;
+
+public:
+	HeapPriorityQueue(int max = DEFAULT_MAX) {
+		pairs = new MinHeap<Pair<int, E>>(max);
+	}
+	~HeapPriorityQueue() {
+		delete pairs;
+	}
+
+	void insert(E element, int priority) {
+		Pair<int, E> p(priority, element);
+		pairs->insert(p);
+	}
+
+	E min() {
+		Pair<int, E> p = pairs->first();
+		return p.value;
+	}
+
+	E removeMin() {
+		Pair<int, E> p = pairs->removeFirst();
+		return p.value;
+	}
+
+	void clear() {
+		pairs->clear();
+	}
+
+	int getSize() {
+		return pairs->getSize();
+	}
+
+	bool isEmpty() {
+		return pairs->isEmpty();
+	}
+
+	void print() {
+		pairs->print();
+	}
+
+};
+
