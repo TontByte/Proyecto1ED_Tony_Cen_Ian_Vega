a80b5224786797b0860c119adb5e028053512691
TontByte <tonycen957@gmail.com>
2026-05-04 17:20:13 -0600

    Primera version preliminar de AreaManager completada (corregir comentarios)

--- AreaManager.h
+++ AreaManager.h
@@ -1,30 +1,60 @@
+/*
+Esta es la clase AreaManager (escrita principalmente por Tony y revisada/mejorada por Ian)
+Es un control de todos las areas
+Permite ejecutar funciones sobre las areas deseadas
+*/
+
 #pragma once
 #include <iostream>
 #include <stdexcept>
+#include <string>
 #include "Area.h"
 #include "ArrayList.h"
 
 using std::cout;
 using std::runtime_error;
 using std::endl;
+using std::string;
 
-class AreaManager{
+class AreaManager {
 private:
-	ArrayList<Area>* areas;
+	ArrayList<Area*> areas;
 
 public:
-	AreaManager() {
-		areas = new ArrayList<Area>();
-	}
+	AreaManager() {}
 
 	~AreaManager() {
-		delete areas;
+		for (areas.goToStart(); !areas.atEnd(); areas.next()) {
+			delete areas.getElement();
+		}
+		areas.clear();
 	}
 
-	bool createArea(string descripcion, string codigo, int cantV) {
-		Area a()
-		areas->insert()
+	AreaManager(const AreaManager&) = delete;
+	AreaManager& operator=(const AreaManager&) = delete;
+
+	void createArea(string descripcion, string codigo, int cantV) {
+		Area* a = new Area(descripcion, codigo, cantV);
+		areas.append(a);
 	}
 
+	//agregar muestreo de servicios relacionados al area
+	//agregar confirmacion en programa principal
+	void modifyAreaVentanillas(int cant, int choiceIndex) {
+		if (choiceIndex < 0 || choiceIndex >= areas.getSize()) {
+			throw runtime_error("Indice seleccionado invalido.");
+		}
+		areas.goToPos(choiceIndex);
+		areas.getElement()->modifyV(cant);
+	}
+
+	void deleteArea(int choiceIndex) { //se utilizara indice para decidir 
+		if (choiceIndex < 0 || choiceIndex >= areas.getSize()) {
+			throw runtime_error("Indice seleccionado invalido.");
+		}
+		areas.goToPos(choiceIndex);
+		Area* a = areas.remove();
+		delete a;
+	}
 };
 
