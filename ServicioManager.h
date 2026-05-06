a65ef798db10d560d50841dee25a8911c7463293
iansamuel2006 <iansamuel2006@gmail.com>
2026-05-06 16:28:19 -0600

    finalizacion de la clase servicio manager

--- ServicioManager.h
+++ ServicioManager.h
@@ -7,7 +7,7 @@ using namespace std;
 
 class ServicioManager {
 private:
-		ArrayList<Servicio> servicios;
+	ArrayList<Servicio> servicios;
 
 public:
 	void agregarServicio(string descripcion, int prioridad, string area) {
@@ -25,3 +25,34 @@ public:
 			cout << "Indice invalido.\n";
 		}
 	}
+
+	void reordenarServicio(int origen, int destino) {
+		if (origen < 0 || origen >= servicios.getSize() || destino < 0 || destino >= servicios.getSize()) {
+			cout << "Indice invalido.\n";
+			return;
+		}
+		//obtener el servicio a mover
+		servicios.goToPos(origen);
+		Servicio temp = servicios.getElement();
+		servicios.remove();
+		//insertar el servicio en la nueva posicion
+		servicios.goToPos(destino);
+		servicios.insert(temp);
+
+		cout << "Servicio reordenado.\n";
+	}
+	void mostrarServicios() {
+		cout << "Servicios:\n";
+		for (int i = 0; i < servicios.getSize(); i++) {
+			servicios.goToPos(i);
+			cout << i << ": " << servicios.getElement() << endl;
+		}
+	}
+	Servicio getServicio(int index) {
+		servicios.goToPos(index);
+		return servicios.getElement();
+	}
+	int size() {
+		return servicios.getSize();
+	}
+};
